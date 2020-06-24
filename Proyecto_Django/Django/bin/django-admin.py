1a044c0379d4de25f78a96b20a1eda8d97276d4b
Walter Huaracha <whuaracha@unsa.edu.pe>
2020-06-24 16:51:50 -0500

    Mi aplicacion Django, primer commit

---
+++ Proyecto_Django/Django/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/walter/WorkSpace/Pweb2/Proyecto_Django/Django/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ Proyecto_Django/ProyectoDestinosTuristicos/Destinos/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Destination(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombreCiudad = models.CharField(max_length = 50) 
+    imagenCiudad = models.ImageField(upload_to='pics')
+    descripcionCiudad = models.TextField()
+    precioTour = models.IntegerField()
+    ofertaTour = models.BooleanField(default=False)
