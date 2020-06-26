06ff8156b4d7e03a9a2fea502f8c4159bfa49d5c
Walter Huaracha <whuaracha@unsa.edu.pe>
2020-06-26 18:41:02 -0500

    Implementando el formulario para agregar destinos turisticos

---
+++ Proyecto_Django/ProyectoDestinosTuristicos/Destinos/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+from .models import Destination
+
+class DestinationForm(forms.ModelForm):
+    class Meta:
+        model = Destination
+        fields = [
+                'nombreCiudad',
+                'imagenCiudad',
+                'descripcionCiudad',
+                'precioTour',
+                'ofertaTour',
+                ]
