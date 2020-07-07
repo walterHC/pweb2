a76bcf2a01249ab72bc22ccb6099ba21a67505a1
Walter Huaracha <whuaracha@unsa.edu.pe>
2020-07-07 17:15:40 -0500

    implementacion del metodo que borra un destino

--- Proyecto_Django/ProyectoDestinosTuristicos/ProyectoDestinosTuristicos/urls.py
+++ Proyecto_Django/ProyectoDestinosTuristicos/ProyectoDestinosTuristicos/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('', include('travello.urls')),
     path('admin/',admin.site.urls),
     path('accounts/', include('Accounts.urls')),
-    path('añadir/', include('Destinos.urls'))
+    path('administrador/', include('Destinos.urls'))
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
