85a5877945dea1769f7e43d61fc2898744850b5a
Walter Huaracha <whuaracha@unsa.edu.pe>
2020-07-09 13:06:28 -0500

    Cambios e identacion en las estructuras de los html de templates

--- Proyecto_Django/ProyectoDestinosTuristicos/Destinos/forms.py
+++ Proyecto_Django/ProyectoDestinosTuristicos/Destinos/forms.py
@@ -15,6 +15,12 @@ class DestinationForm(forms.ModelForm):
 class RawDestinationForm(forms.Form):
     nombreCiudad = forms.CharField()
     imagenCiudad = forms.ImageField()
-    descripcionCiudad = forms.CharField()
+    descripcionCiudad = forms.CharField(
+                widget= forms.Textarea(
+                    attrs={
+                        'cols':'10',
+                    }
+                )
+            )
     precioTour = forms.IntegerField()
     ofertaTour = forms.BooleanField()
